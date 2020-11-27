24f555027530bd1dfb57e46ad76f7e961ce4d597
Shadowmaple <1142319190@qq.com>
2020-11-27 22:00:25 +0800

    login finish

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ App({
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
 
-    this.login()
+    this.checkLogin(res => {
+      console.log('is_login: ', res.is_login)
+      if (!res.is_login) {
+        this.login()
+      }
+    })
   },
   // 登录
   login: function () {
@@ -25,37 +30,63 @@ App({
             // 保存到全局数据
             this.globalData.token = res.data.token
             // 保存到缓存
-            // wx.setStorage({
-            //   data: res.data.token,
-            //   key: 'token',
-            // })
+            wx.setStorage({
+              data: res.data.token,
+              key: 'token',
+            })
           }
         })
       }
     })
   },
+  checkLogin: function (callback) {
+    var token = this.globalData.token
+    if (!token) {
+      // 从数据库中获取 token
+      token = wx.getStorageSync('token')
+      if (token) {
+        this.globalData.token = token
+      } else {
+        callback({
+          is_login: false
+        })
+        return
+      }
+    }
+    wx.request({
+      url: 'http://127.0.0.1:3000/check_login',
+      data: {
+        token: token
+      },
+      success: res => {
+        callback({
+          is_login: res.data.is_login
+        })
+      }
+    })
+  },
   // 获取用户信息
-  // getUserInfo: function () {
-  //   wx.getSetting({
-  //     success: res => {
-  //       if (res.authSetting['scope.userInfo']) {
-  //         // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-  //         wx.getUserInfo({
-  //           success: res => {
-  //             // 可以将 res 发送给后台解码出 unionId
-  //             this.globalData.userInfo = res.userInfo
+  getUserInfo: function () {
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
 
-  //             // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-  //             // 所以此处加入 callback 以防止这种情况
-  //             if (this.userInfoReadyCallback) {
-  //               this.userInfoReadyCallback(res)
-  //             }
-  //           }
-  //         })
-  //       }
-  //     }
-  //   })
-  // },
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
   globalData: {
     userInfo: null,
     token: null
